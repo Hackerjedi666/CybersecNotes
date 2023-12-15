e4c0ed477664d45bf0b7ea03b378980c2171643c
abhimanyuGupta <abhimanyu.gupta@rubberduckypro.com>
2023-12-16 01:32:11 +0530

    oswe

--- oswe/scripts_important/scripts.cs
+++ oswe/scripts_important/scripts.cs
@@ -1,6 +1,6 @@
 //If you see this you can modify it to the below script
 
-DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePointsjava.net.
+DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePointsjava.net
 
 //Script to modify the assembly to disable optimizations
 Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations |
