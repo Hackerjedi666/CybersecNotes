1f7e613e1ee02d8ec170d672beaf577ee2b96a32
hackerjedi <hackerjedi@hackerjedi.hackerjedi>
2024-09-12 17:55:04 +0530

    testing

--- oscp/scripts/zoneTransfer.sh
+++
@@ -1,5 +0,0 @@
-echo "Enter the domain you wanna do zone transfer for :"; 
-read domain;
-for dnserver in $(host -t ns $domain | cut -d " " -f 4); do host -l megacorpone.com $dnserver; done | grep -v "not found" | grep -v "Transfer failed"
-
-
